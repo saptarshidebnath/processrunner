46fdd1af1e362f213e29f05826e0239c2de3166f
Saptarshi Debnath <saptarshi.devnath@gmail.com>
2016-12-08 00:04:39 -0500

    Wrote test case for the utilities class

---
+++ src/test/java/com/saptarshidebnath/processrunner/lib/utilities/UtilitiesTest.java
@@ -0,0 +1,36 @@
+package com.saptarshidebnath.processrunner.lib.utilities;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.File;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+/**
+ * Created by saptarshi on 12/7/2016.
+ */
+public class UtilitiesTest {
+    private File tempFile;
+
+    @Before
+    public void setUp() throws Exception {
+        this.tempFile = Utilities.createTempLogDump();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        this.tempFile.delete();
+    }
+
+    @Test
+    public void createTempLogDump() throws Exception {
+        assertThat(
+                "Checking if File exists",
+                this.tempFile.exists(),
+                is(true));
+    }
+
+}
